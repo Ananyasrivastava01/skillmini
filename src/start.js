d076f9035e3986ee6132a14a5cdea3431a75f935
Ananyasrivastava01 <srivastavaananya113@gmail.com>
2025-10-05 15:44:18 +0530

    some changes

---
+++ src/start.js
@@ -0,0 +1,6 @@
+// Run migrations and seed at runtime, then start the server
+import '../scripts/migrate.js';
+import '../scripts/seed.js';
+import './server.js';
+
+
--- src/views/pages.js
+++ src/views/pages.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 export const pagesRouter = express.Router();
 
+// Add this new route to handle the redirect
+// This will redirect any traffic from the root URL ('/') to ('/tickets')
+pagesRouter.get('/', (req, res) => res.redirect('/tickets'));
+
 // Minimal UI to exercise APIs. Uses localStorage token via script.
 pagesRouter.get('/tickets', (req, res) => res.render('tickets'));
 pagesRouter.get('/tickets/new', (req, res) => res.render('new'));
 pagesRouter.get('/tickets/:id', (req, res) => res.render('show'));
-
-
